d52b14f1d52a3f116d502f64e8328d2778773a29
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2023-02-02 19:37:18 -0800

    save figures

--- src/fastq_qc.py
+++ src/fastq_qc.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 
-import argparse
 import pandas as pd
 import seaborn as sns
 from Bio import SeqIO
+from matplotlib import pyplot as plt
 
 
 def calculate_score(input):
@@ -24,20 +25,25 @@ def create_graphs(df, density=True):
         sns.kdeplot(df, x="quality_score", hue="base").set(
             title="Quality distribution per base"
         )
+        plt.savefig("quality_distribution_per_base.png")
     else:
         sns.set(font_scale=0.6)
         sns.boxplot(x="position_in_read", y="quality_score")
+        plt.savefig("quality_boxplot.png")
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="""fastq QC"""
+    parser = argparse.ArgumentParser(description="""fastq QC""")
+    parser.add_argument(
+        "-i", "--input", required=True, help="input file in fastq format"
     )
-    parser.add_argument("-i", "--input", required=True, help="input file in fastq format")
     args = parser.parse_args()
     input_file = args.input
     df_list = []
-    qual_scores, qual_bases = calculate_score(input_file)[0], calculate_score(input_file)[1]
+    qual_scores, qual_bases = (
+        calculate_score(input_file)[0],
+        calculate_score(input_file)[1],
+    )
     for i in range(0, len(qual_scores)):
         for j in range(1, len(qual_scores[1])):
             df_list.append(
@@ -50,7 +56,9 @@ def main():
             )
     qual_df = pd.DataFrame(df_list)
     # get summary stats
-    print("summary statistics of read qualities:\n",qual_df["quality_score"].describe())
+    print(
+        "summary statistics of read qualities:\n", qual_df["quality_score"].describe()
+    )
     # get density plot per base
     create_graphs(qual_df, density=True)
     # get box plot of quality scores

87575a7275ab141feda6ffc5f3f6d921803b5fd3
Mariaelf97 <mahmadijeshvag6700@sdsu.edu>
2023-03-30 10:22:48 -0700

    code reformatted

--- src/fastq_qc.py
+++ src/fastq_qc.py
@@ -1,69 +1,35 @@
 #!/usr/bin/env python3
 import argparse
+import csv
 import sys
 
-import pandas as pd
-import seaborn as sns
 from Bio import SeqIO
-from matplotlib import pyplot as plt
 
 
-def calculate_score(input_file_path):
-    """function to get quality score for each base in all reads"""
-    qual_list = []
-    base_list = []
-    for record in SeqIO.parse(input_file_path, "fastq"):
-        qual_score = record.letter_annotations["phred_quality"]
-        qual_base = record.seq
-        qual_list.append(qual_score)
-        base_list.append(qual_base)
-    return qual_list, base_list
-
-
-def create_graphs(df, density=True):
-    """function to plot quality scores"""
-    if density:
-        sns.kdeplot(df, x="quality_score", hue="base").set(
-            title="Quality distribution per base"
-        )
-        plt.savefig("quality_distribution_per_base.png")
-    else:
-        sns.boxplot(df, x="position_in_read", y="quality_score")
-        plt.tick_params(axis="both", which="major", labelsize=5)
-        plt.savefig("quality_boxplot.png")
+def read_fasta_file(path_to_file):
+    fasta_file = SeqIO.parse(path_to_file, "fasta")
+    return fasta_file
 
 
 def main():
-    parser = argparse.ArgumentParser(description="""fastq QC""")
+    parser = argparse.ArgumentParser(
+        description=""" program to print all gene names longer than
+     2000 nucleotides in a multi-fasta file"""
+    )
     parser.add_argument(
-        "-i", "--input", required=True, help="input file in fastq format"
+        "-i", "--input", required=True, help="input file in multi fasta format"
     )
     args = parser.parse_args()
     input_file = args.input
-    df_list = []
-    qual_scores, qual_bases = (
-        calculate_score(input_file)[0],
-        calculate_score(input_file)[1],
-    )
-    for i in range(0, len(qual_scores)):
-        for j in range(1, len(qual_scores[1])):
-            df_list.append(
-                {
-                    "read_number": i,
-                    "position_in_read": j,
-                    "base": qual_bases[i][j],
-                    "quality_score": qual_scores[i][j],
-                }
-            )
-    qual_df = pd.DataFrame(df_list)
-    # get summary stats
-    print(
-        "summary statistics of read qualities:\n", qual_df["quality_score"].describe()
-    )
-    # get density plot per base
-    create_graphs(qual_df, density=True)
-    # get box plot of quality scores
-    create_graphs(qual_df, density=False)
+    fasta_file = read_fasta_file(input_file)
+    # print all gene names
+    gene_list = []
+    for r in fasta_file:
+        if len(r.seq) > 2000:
+            gene_list.append(r.id)
+    with open("genes_greater_than_2000_bps.csv", "w") as file:
+        wr = csv.writer(file)
+        wr.writerow(gene_list)
 
 
 if __name__ == "__main__":

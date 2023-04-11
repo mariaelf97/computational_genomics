014fb285d7b6078d7f48352eee6311943d7156f6
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2023-04-10 21:43:37 -0700

    bash script to run GWAS analyses

--- src/population_genomics/GWAS.sh
+++ src/population_genomics/GWAS.sh
@@ -1,4 +1,6 @@
 # to check loci in hardy weinberg equilibrium
 vcftools --gzvcf chr16.vcf.gz --hardy
-# this will create a file which has chromosoem information, observed homozygosity, heterozygosity, chi square value HWE,
+# this will create a file which has chromosome information, observed homozygosity, heterozygosity, chi square value HWE,
 # p-value of HWE
+# This will take out all sites not in hwe and allow not less or more than 2 alleles per site
+vcftools --gzvcf chr16.vcf.gz --hwe 0.05 --min-alleles 2 --max-alleles 2 --recode --stdout | gzip -c > chr16_hwe.vcf.gz

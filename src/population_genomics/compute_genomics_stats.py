30a59ec612740d2f5b6070da87e1a70223939449
Mariaelf97 <mahmadijeshvag6700@sdsu.edu>
2023-04-12 15:03:27 -0700

    changed argparser help summary to out.frq instead of out.freq

--- src/population_genomics/compute_genomics_stats.py
+++ src/population_genomics/compute_genomics_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import argparse
-import io
 import sys
 
 import pandas as pd
@@ -14,21 +13,29 @@ def main():
         "-i1", "--input1", required=True, help="vcftools out.het output"
     )
     parser.add_argument(
-        "-i2", "--input2", required=True, help="vcftools out.freq output"
+        "-i2", "--input2", required=True, help="vcftools out.frq output"
     )
 
     args = parser.parse_args()
     out_het = args.input1
     out_freq = args.input2
-    vcf_input = pd.read_csv(out_het, sep='\t')
-    vcf_input['heterozygosity'] = (vcf_input['N_SITES'] - vcf_input['O(HOM)']) / vcf_input['N_SITES']
-    heterozygosity_per_ind = vcf_input[['INDV','heterozygosity']]
-    heterozygosity_per_ind.to_csv("heterozygosity.tsv", sep ="\t")
-    allele_freq_input = pd.read_csv(out_freq, sep='\t')
-    allele_freq_output = allele_freq_input[['CHROM','{FREQ}']]
-    allele_freq_output["allele_2_freq"] = 1- allele_freq_output['{FREQ}']
-    allele_freq_output.rename(columns={'CHROM': 'locus', '{FREQ}': 'allele_1_freq',
-                                       "allele_2_freq": "allele_2_freq"}, inplace=True)
+    vcf_input = pd.read_csv(out_het, sep="\t")
+    vcf_input["heterozygosity"] = (
+        vcf_input["N_SITES"] - vcf_input["O(HOM)"]
+    ) / vcf_input["N_SITES"]
+    heterozygosity_per_ind = vcf_input[["INDV", "heterozygosity"]]
+    heterozygosity_per_ind.to_csv("heterozygosity.tsv", sep="\t")
+    allele_freq_input = pd.read_csv(out_freq, sep="\t")
+    allele_freq_output = allele_freq_input[["CHROM", "{FREQ}"]]
+    allele_freq_output["allele_2_freq"] = 1 - allele_freq_output["{FREQ}"]
+    allele_freq_output.rename(
+        columns={
+            "CHROM": "locus",
+            "{FREQ}": "allele_1_freq",
+            "allele_2_freq": "allele_2_freq",
+        },
+        inplace=True,
+    )
     allele_freq_output.to_csv("allele_frequency.tsv", sep="\t")
 
 
